822f7a6d3b3e686c9df98530df22d8a33eb3c104
dominickfabry <70919349+dominickfabry@users.noreply.github.com>
2022-07-06 09:03:30 -0600

    started_blog_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   #Routes https://website.com/pages/about  to shorter https://website.com/about.com
   get 'about', to: 'pages#about'
   #Set default page for https://website.com
